744d13550d23eab534d785178d695b8368e89e02
Daniel Fahrni <daniel.fahrni@bzz.ch>
2023-06-08 11:01:55 +0200

    refactoring of functions

--- jump-and-run-game/src/maps/map_one.js
+++ jump-and-run-game/src/maps/map_one.js
@@ -1,48 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 const MapOne = () => {
-  const [bottomPositions, setBottomPosition] = useState(50);
-  
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === ' ') {
-        jump();
-      }
-      if (e.key === 'v') {
-        enterFullScreen();
-      }
-      if (e.key === 'Enter') {
-        enterFullScreen();
-        startgame();
-      }
-    };
 
-    document.addEventListener('keydown', handleKeyDown);
+  let blockPositions;
+  let bottomPositions;
+  let setBottomPosition;
+  let rightPositions;
+  let leftPositions;
+  let figur;
+  let blocks;
 
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, );
-
-  const jump = () => {
-    // Figur hochsetzen
-    // warten (z.B. 10 ms)
-    // Figur runtersetzen (entscheiden wie tier runter)
-    const figur = document.querySelector('#Spielfigur');
-    let leftPositions = figur.offsetLeft;
-    let rightPositions = leftPositions + figur.offsetWidth;
-    const blocks = document.querySelectorAll('.block');
-
-    let blockPositions = [];
-    let blockdiff = [];
+  function fall() {
+    setTimeout(() => {
     
-    const hitbox = () => {
-      if (blockPositions.some((position) => screenheight-position.top === bottomPositions && position.left <= rightPositions && position.right >= leftPositions)) {
-        return true;
-      } else {
-        return false; 
-      }
-    };
+    }, 1000);
+  };
+
+  const hitbox = () => {
+    // calculate  positions
+    figur = document.querySelector('#Spielfigur');
+    leftPositions = figur.offsetLeft;
+    rightPositions = leftPositions + figur.offsetWidth;
+
+    let screenheight = window.screen.height;
+
+    return calculateBlockPositions().some((position) => 
+    screenheight-position.top === bottomPositions && 
+      position.left <= rightPositions && 
+      position.right >= leftPositions);
+  };
+
+  const calculateBlockPositions = () => {
+    blockPositions = [];
+    blocks = document.querySelectorAll('.block');
     blocks.forEach((block) => {
       let topPosition = block.offsetTop;
       let bottomPosition = topPosition + block.offsetHeight;
@@ -54,54 +44,74 @@ const MapOne = () => {
         left: leftPosition,
         right: rightPosition,
       };
-  
-      blockPositions.push(positionObj);
 
-      blockdiff.push(rightPosition - leftPosition);
+      blockPositions.push(positionObj);
     });
-    function fall() {
-      setTimeout(() => {
-       
-      }, 1000);
-    }
-  
-    let screenheight = window.screen.height;
-    if (hitbox()) {
-      setBottomPosition((prevBottomPosition) => prevBottomPosition + 150);
-    } 
+    return blockPositions;
+  };
+
+  const jump = () => {
+    if (!hitbox()) return;
+
+    // Figur hochsetzen
+    setBottomPosition(prevBottomPosition => prevBottomPosition + 150);
   };
 
   function enterFullScreen() {
     let element = document.documentElement;
-    if (element.requestFullscreen) {
+    if (element.requestFullscreen)
       element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) {
+    else if (element.mozRequestFullScreen)
       element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
+    else if (element.webkitRequestFullscreen)
       element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) {
+    else if (element.msRequestFullscreen)
       element.msRequestFullscreen();
-    }
   }
 
   const startgame = () => {
     const blocks = document.querySelectorAll('.block');
-  
+
     blocks.forEach((block) => {
       let currentPosition = parseFloat(block.offsetLeft);
       const animateBlock = () => {
         currentPosition -= 1; 
         block.style.left = `${currentPosition}px`;
-  
+
         if (currentPosition > -block.offsetWidth) {
           requestAnimationFrame(animateBlock);
         }
       };
-  
+
       animateBlock();
     });
   };
-  
+
+  const handleKeyDown = (e) => {
+    switch (e.key) {
+      case ' ': 
+        jump();
+        break;
+      case 'v': 
+        enterFullScreen();
+        break;
+      case 'Enter':
+      default:
+        enterFullScreen();
+        startgame();
+        break;
+    }
+  };
+
+  [bottomPositions, setBottomPosition] = useState(50);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
   return (
     <div id="maponediv">
       <div id="Spielfigur" style={{ bottom: `${bottomPositions}px` }}></div>
